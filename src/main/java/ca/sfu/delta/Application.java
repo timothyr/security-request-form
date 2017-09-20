49280343266abf5fa5118f4ff8e7cc850187a5f0
Lance Hannestad <lhannest@gmail.com>
2017-09-20 10:02:13 -0700

    issue #1 authentication

--- src/main/java/ca/sfu/delta/Application.java
+++ src/main/java/ca/sfu/delta/Application.java
@@ -1,5 +1,6 @@
 package ca.sfu.delta;
 
+import ca.sfu.delta.data.Database;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,12 +8,13 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {
         "ca.sfu.delta.ui",
-        "ca.sfu.delta.data"
+        "ca.sfu.delta.data",
+        "ca.sfu.delta.auth",
 })
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	    SpringApplication.run(Application.class, args);
 	}
 
 }
