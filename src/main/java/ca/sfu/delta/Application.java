95fb32168feef86a714e9875edd7799f245535e0
Lance Hannestad <lhannest@gmail.com>
2017-09-28 15:50:14 -0700

    Add component scan back in (it's empty) so we don't forget about it

--- src/main/java/ca/sfu/delta/Application.java
+++ src/main/java/ca/sfu/delta/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {})
 public class Application {
 
 	public static void main(String[] args) {
