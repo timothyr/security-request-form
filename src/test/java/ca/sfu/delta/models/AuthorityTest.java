933ded044f869745074be8fbba6bf0f0b77caa0b
Harjot Singh <hlotey@sfu.ca>
2017-10-02 13:44:30 -0700

    Authority Class with getters and setters.

---
+++ src/test/java/ca/sfu/delta/models/AuthorityTest.java
@@ -0,0 +1,48 @@
+package ca.sfu.delta.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AuthorityTest {
+    @Test
+    public void getAuthId() throws Exception {
+    }
+
+    @Test
+    public void setAuthId() throws Exception {
+    }
+
+    @Test
+    public void getName() throws Exception {
+    }
+
+    @Test
+    public void setName() throws Exception {
+    }
+
+    @Test
+    public void getPhone() throws Exception {
+    }
+
+    @Test
+    public void setPhone() throws Exception {
+    }
+
+    @Test
+    public void getSfuId() throws Exception {
+    }
+
+    @Test
+    public void setSfuId() throws Exception {
+    }
+
+    @Test
+    public void getSignature() throws Exception {
+    }
+
+    @Test
+    public void setSignature() throws Exception {
+    }
+
+}
