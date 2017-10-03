d1f4b8715c502b07423ec266e1e5a8c515c61850
Lance Hannestad <lhannest@gmail.com>
2017-10-02 19:28:48 -0700

    Create repositories

---
+++ src/main/java/ca/sfu/delta/repository/FormRepository.java
@@ -0,0 +1,8 @@
+package ca.sfu.delta.repository;
+
+import ca.sfu.delta.models.FormData;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FormRepository extends CrudRepository<FormData, Long> {
+
+}
