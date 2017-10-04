18a74e21144800d771980cdf9e861da4ad9365bb
Lance Hannestad <lhannest@gmail.com>
2017-10-03 17:42:55 -0700

    Autowire FormRepository

--- src/main/java/ca/sfu/delta/controllers/RequestsController.java
+++ src/main/java/ca/sfu/delta/controllers/RequestsController.java
@@ -1,16 +1,30 @@
 package ca.sfu.delta.controllers;
 
+import ca.sfu.delta.models.FormData;
 import ca.sfu.delta.repository.FormRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class RequestsController {
 
+    @Autowired
+    FormRepository formRepository;
+
     @RequestMapping("/requests")
     public String requests(Model model) {
-        model.addAttribute("requests", FormRepository.listAllRequests());
+        List<FormData> forms = new ArrayList<FormData>();
+        for (FormData form : formRepository.findAll()) {
+            forms.add(form);
+        }
+
+        model.addAllAttributes(forms);
+
         return "requests";
     }
 
