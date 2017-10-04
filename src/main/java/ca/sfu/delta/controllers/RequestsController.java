90013c4fe7726ce5670d9378bd3796dca6768c8b
raymonde <raymonde@sfu.ca>
2017-10-03 17:32:28 -0700

    Change RequestController - BUILD NEEDS findall

--- src/main/java/ca/sfu/delta/controllers/RequestsController.java
+++ src/main/java/ca/sfu/delta/controllers/RequestsController.java
@@ -1,13 +1,16 @@
 package ca.sfu.delta.controllers;
 
+import ca.sfu.delta.repository.FormRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RequestsController {
 
     @RequestMapping("/requests")
-    public String requests() {
+    public String requests(Model model) {
+        model.addAttribute("requests", FormRepository.listAllRequests());
         return "requests";
     }
 
