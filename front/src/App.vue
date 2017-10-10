db868d2735cd441f5465bbb47076cc8ef683a2b7
raymonde <raymonde@sfu.ca>
2017-10-10 00:06:18 -0700

    Add RequestForm Component

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <AppHeader/>
+    <RequestForm/>
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader'
+import RequestForm from './components/RequestForm'
 
 export default {
   name: 'app',
   components: {
-    AppHeader
+    AppHeader,
+    RequestForm
   }
 }
 </script>
