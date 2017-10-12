2795f30f494d7455b9019882900e61285496c1ec
raymonde <raymonde@sfu.ca>
2017-10-11 21:27:24 -0700

    Configure vue-router

--- front/src/main.js
+++ front/src/main.js
@@ -2,14 +2,29 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import RequestForm from './components/RequestForm.vue'
+import Requests from './components/Requests.vue'
 
 require('bootstrap/dist/css/bootstrap.min.css');
-require('./assets/css/style.css')
+require('./assets/css/style.css');
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+/* set up vue router */
+const routes = [
+	{ path:'/', component: RequestForm },
+	{ path:'/security', component: Requests }
+];
+const router = new VueRouter({
+	routes,
+	mode: 'history'
+});
 
 /* eslint-disable no-new */
 new Vue({
+  router,
   el: '#app',
   template: '<App/>',
   components: { App }
