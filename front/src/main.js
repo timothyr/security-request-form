02bdea247095305e979d3a7466b57f953af0618e
rcretu <rcretu@sfu.ca>
2017-10-15 11:46:03 -0700

    Add bare security view

--- front/src/main.js
+++ front/src/main.js
@@ -5,20 +5,22 @@ import App from './App'
 import VueRouter from 'vue-router'
 import RequestForm from './components/RequestForm.vue'
 import Requests from './components/Requests.vue'
+import SecurityView from './components/SecurityView.vue'
 import PageNotFound from './components/PageNotFound.vue'
 
 require('bootstrap/dist/css/bootstrap.min.css');
 require('font-awesome/css/font-awesome.css');
 require('./assets/css/style.css');
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 /* set up vue router */
 const routes = [
-	{ path:'/', component: RequestForm },
-	{ path:'/security', component: Requests },
-	{ path:'*', component: PageNotFound }
+  { path:'/', component: RequestForm },
+  { path:'/security', component: Requests },
+  { path:'/security/request', component: SecurityView},
+  { path:'*', component: PageNotFound }
 ];
 const router = new VueRouter({
 	routes,
@@ -31,4 +33,4 @@ new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
-})
+});
