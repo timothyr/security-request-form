e1fcda99be0263b0da2be152acc5fb089ee2a390
rcretu <rcretu@sfu.ca>
2017-10-20 21:32:43 -0700

    Migrate security view to new architecture

--- src/main/java/ca/sfu/delta/controllers/SecurityViewController.java
+++ src/main/java/ca/sfu/delta/controllers/SecurityViewController.java
@@ -1,4 +1,23 @@
 package ca.sfu.delta.controllers;
 
+import ca.sfu.delta.repository.FormRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class SecurityViewController {
+
+	@Autowired
+	FormRepository formRepository;
+
+	@RequestMapping("/security/request/{id}")
+	public String securityview(@PathVariable(value = "id", required = true) long id, Model model) {
+		model.addAttribute("securityview", formRepository.findOne(id));
+		return "securityview";
+	}
+
 }
