5c7ba225f128b1adc6f7f32e1902ff58e36c1338
Harjot Singh <hlotey@sfu.ca>
2017-10-23 16:37:31 -0700

    Add appropriate dependencies for Emails

--- src/main/java/ca/sfu/delta/models/SendEmail.java
+++ src/main/java/ca/sfu/delta/models/SendEmail.java
@@ -1,10 +1,9 @@
 package ca.sfu.delta.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 
 import javax.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 public class SendEmail {
     private String firstName;
