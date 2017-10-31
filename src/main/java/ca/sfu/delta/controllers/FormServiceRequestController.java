f0fdc04972f6467ef5f0fcfbf2b9a73c1d4f6190
Tim Robertson <timr@sfu.ca>
2017-10-30 23:11:30 -0700

    Submit GET request returns relative link to submitted form
    Using formFromTokenURL + token
    e.g. /api/form/get/user/4cBvW4mvZz7Zw1A0Pp0x6WqHR3FXse

--- src/main/java/ca/sfu/delta/controllers/FormServiceRequestController.java
+++ src/main/java/ca/sfu/delta/controllers/FormServiceRequestController.java
@@ -210,7 +210,8 @@ public class FormServiceRequestController extends WebMvcConfigurerAdapter {
                 System.out.println("Could not send Email. Please ensure all the parameters are valid.");
             }
 	        System.out.println("Successfully saved Form with requestID = " + form.getId() + " and token = " + token);
-	        return String.valueOf(token);
+            String returnLink = formFromTokenURL + token;
+	        return returnLink;
         } else {
             System.out.println("Failed to save Form");
             return "ERROR: form didn't save";
