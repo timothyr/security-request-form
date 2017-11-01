86fcfdcdb02b62059e5572f55b23a4ee05e3865b
raymonde <raymonde@sfu.ca>
2017-11-01 03:53:19 -0700

    Fix date formatting, hardcode status

--- src/main/resources/static/js/datetime.js
+++ src/main/resources/static/js/datetime.js
@@ -1,15 +1,14 @@
-function getArrayDateTimes( aString ) {
-    var dates = [];
+function updateDateTime(aString){
     var array = aString.split(", ");
+    var dates = [];
 
     for(i = 0 ; i < array.length ; i++){
+        var dateTime = array[i].split("-");
 
-        var dateTime = array[i].split(" ");
         dates.push( {
-            startDate:dateTime[0],
-            startTime:dateTime[1],
-            endDate:dateTime[2],
-            endTime:dateTime[3]
+            start:dateTime[0],
+            end:dateTime[1],
+            hours: "1.5",
         } );
     }
     return dates;
